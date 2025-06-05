29557ca640adc83c325e7827fa7717c52b95bc5e
chines05 <gpalmenara@gmail.com>
2025-06-05 12:42:24 -0300

    Fix: Atualiza o número de telefone na função handleWhatsAppClick para o correto

--- src/utils/handleWhatsAppClick.jsx
+++ src/utils/handleWhatsAppClick.jsx
@@ -1,6 +1,6 @@
 export function handleWhatsAppClick() {
   window.open(
-    "https://api.whatsapp.com/send?phone=554798828-8826&text=Olá!%0AEstou%20em%20busca%20de%20uma%20solução%20para%20automação%20comercial.%0AGostaria%20de%20saber%20mais%20sobre%20as%20opções%20disponíveis.",
+    "https://api.whatsapp.com/send?phone=55689229-4357&text=Olá!%0AEstou%20em%20busca%20de%20uma%20solução%20para%20automação%20comercial.%0AGostaria%20de%20saber%20mais%20sobre%20as%20opções%20disponíveis.",
     "_blank"
   );
 }
